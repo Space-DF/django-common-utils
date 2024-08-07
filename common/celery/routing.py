61acd919584402c3177e747323e1cc8be1f83835
pikann <hvhai22@gmail.com>
2024-08-07 14:59:45 +0700

    feat: merge space service to auth service

--- common/celery/routing.py
+++ common/celery/routing.py
@@ -11,35 +11,36 @@ def setup_synchronous_model_task_routing():
     if celery_app.conf.task_routes is None:
         celery_app.conf.task_routes = {}
 
-    for model_name in settings.CLONE_MODELS:
-        celery_app.conf.task_queues = celery_app.conf.task_queues + (
-            Queue(
-                f"{settings.SERVICE_NAME}_update_{model_name}",
-                exchange=Exchange(f"update_{model_name}", type="fanout"),
-                routing_key=f"update_{model_name}",
-                queue_arguments={
-                    "x-single-active-consumer": True,
-                },
-            ),
-            Queue(
-                f"{settings.SERVICE_NAME}_delete_{model_name}",
-                exchange=Exchange(f"delete_{model_name}", type="fanout"),
-                routing_key=f"delete_{model_name}",
-                queue_arguments={
-                    "x-single-active-consumer": True,
-                },
-            ),
-        )
-        update_task_name = f"spacedf.tasks.update_{model_name}"
-        celery_app.conf.task_routes[update_task_name] = {
-            "queue": f"{settings.SERVICE_NAME}_update_{model_name}",
-            "routing_key": f"update_{model_name}",
-        }
-        delete_task_name = f"spacedf.tasks.delete_{model_name}"
-        celery_app.conf.task_routes[delete_task_name] = {
-            "queue": f"{settings.SERVICE_NAME}_delete_{model_name}",
-            "routing_key": f"delete_{model_name}",
-        }
+    if hasattr(settings, "CLONE_MODELS"):
+        for model_name in settings.CLONE_MODELS:
+            celery_app.conf.task_queues = celery_app.conf.task_queues + (
+                Queue(
+                    f"{settings.SERVICE_NAME}_update_{model_name}",
+                    exchange=Exchange(f"update_{model_name}", type="fanout"),
+                    routing_key=f"update_{model_name}",
+                    queue_arguments={
+                        "x-single-active-consumer": True,
+                    },
+                ),
+                Queue(
+                    f"{settings.SERVICE_NAME}_delete_{model_name}",
+                    exchange=Exchange(f"delete_{model_name}", type="fanout"),
+                    routing_key=f"delete_{model_name}",
+                    queue_arguments={
+                        "x-single-active-consumer": True,
+                    },
+                ),
+            )
+            update_task_name = f"spacedf.tasks.update_{model_name}"
+            celery_app.conf.task_routes[update_task_name] = {
+                "queue": f"{settings.SERVICE_NAME}_update_{model_name}",
+                "routing_key": f"update_{model_name}",
+            }
+            delete_task_name = f"spacedf.tasks.delete_{model_name}"
+            celery_app.conf.task_routes[delete_task_name] = {
+                "queue": f"{settings.SERVICE_NAME}_delete_{model_name}",
+                "routing_key": f"delete_{model_name}",
+            }
 
 
 def setup_organization_task_routing():

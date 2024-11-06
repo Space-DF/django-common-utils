436358d8124c0873189ef1724b5682c02c29f65b
Nguyen Minh Ngoc <ngoc.nguyen@digitalfortress.dev>
2024-11-06 10:04:39 +0700

    chore: update get devices api

--- common/celery/constants.py
+++ common/celery/constants.py
@@ -1,2 +1,3 @@
 AUTH_SERVICE = "auth_service"
 AUTH_SERVICE_OAUTH_CREDENTIALS_CREATION = f"{AUTH_SERVICE}.oauth_credentials_creation"
+AUTH_SERVICE_ADD_OR_REMOVE_DEVICE = f"{AUTH_SERVICE}.add_or_remove_device"

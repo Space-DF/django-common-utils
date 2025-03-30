743d4691b6ad05b6a10a9d1e95c3967d8347acef
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2025-03-30 14:47:53 +0700

    fix: handle exception

--- common/utils/send_otp_email.py
+++ common/utils/send_otp_email.py
@@ -1,9 +1,10 @@
 import secrets
+import smtplib
 import string
 
-from django.conf import settings
 from django.core.cache import cache  # Import Django's Redis cache
 from django.core.mail import send_mail
+from rest_framework.exceptions import ValidationError
 
 OTP_EXPIRY_SECONDS = 600  # 10 minutes
 
@@ -13,26 +14,36 @@ def generate_otp(length=6):
     return "".join(secrets.choice(string.digits) for _ in range(length))
 
 
-def send_otp_email(user_email):
+def send_otp_email(sender, user_email):
     """Send OTP to user and store it in Redis."""
-    otp_code = generate_otp()
+    try:
+        otp_code = generate_otp()
 
-    # Store OTP in Redis with a 10-minute expiration
-    cache.set(f"otp_{user_email}", otp_code, timeout=OTP_EXPIRY_SECONDS)
+        # Store OTP in Redis with a 10-minute expiration
+        cache.set(f"otp_{user_email}", otp_code, timeout=OTP_EXPIRY_SECONDS)
 
-    # Email content
-    subject = "Your One-Time Sign-In Code"
-    message = f"""
-    Hello,
+        # Email content
+        subject = "Your One-Time Sign-In Code"
+        message = f"""
+        Hello,
 
-    Your one-time sign-in code is: {otp_code}
+        Your one-time sign-in code is: {otp_code}
 
-    This code will expire in 10 minutes.
+        This code will expire in 10 minutes.
 
-    Best regards,
-    Digital Fortress
-    """
+        Best regards,
+        Digital Fortress
+        """
 
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email])
+        send_mail(subject, message, sender, [user_email])
 
-    return otp_code
+        return otp_code
+
+    except smtplib.SMTPDataError:
+        raise ValidationError({"error": "Email address is not verified."})
+
+    except smtplib.SMTPException as e:
+        raise ValidationError({"error": str(e)})
+
+    except Exception as e:
+        raise ValidationError({"error": f"Unexpected Error: {e}"})

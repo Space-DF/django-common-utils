1bf05f2a514526e145fd03c13de54dd613a9d4b7
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2025-05-12 09:37:50 +0700

    feature: check permission when change space

--- common/permissions/permission_classes.py
+++ common/permissions/permission_classes.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.core.cache import cache
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import BasePermission
 
 
@@ -7,3 +9,12 @@ class HasAPIKey(BasePermission):
         spacedf_key = request.headers.get("x-api-key", None)
         # TODO: need model for this
         return settings.ROOT_API_KEY == spacedf_key
+
+
+class HasChangePermission(BasePermission):
+    def has_permission(self, request, view):
+        user_id = request.headers.get("X-User-ID", None)
+        data = cache.get(f"space_permissions_{user_id}")
+        if not data:
+            raise AuthenticationFailed("Permission changed. Please refresh token.")
+        return True

7df6a681c0a00baa009f2e4b8b47424e5c09d32b
lethanhdat762003 <dat.le@df.technology>
2026-05-11 16:02:36 +0700

    fix: avoid spamming email

--- common/utils/send_email.py
+++ common/utils/send_email.py
@@ -1,3 +1,5 @@
+import re
+
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 from django.conf import settings
@@ -11,27 +13,32 @@ client = boto3.client(
 )
 
 
-def send_email(sender, user_emails, subject, html_message):
+def send_email(sender, user_emails, subject, html_message, text_message=None, reply_to=None):
     """Send email via Amazon SES API using boto3."""
 
     if isinstance(user_emails, str):
         user_emails = [user_emails]
 
-    try:
-        response = client.send_email(
-            Source=sender,
-            Destination={"ToAddresses": user_emails},
-            Message={
-                "Subject": {"Data": subject, "Charset": "UTF-8"},
-                "Body": {
-                    "Html": {"Data": html_message, "Charset": "UTF-8"},
-                    "Text": {
-                        "Data": "This email requires an HTML-compatible client.",
-                        "Charset": "UTF-8",
-                    },
-                },
+    if text_message is None:
+        text_message = _html_to_plain_text(html_message)
+
+    kwargs = {
+        "Source": sender,
+        "Destination": {"ToAddresses": user_emails},
+        "Message": {
+            "Subject": {"Data": subject, "Charset": "UTF-8"},
+            "Body": {
+                "Html": {"Data": html_message, "Charset": "UTF-8"},
+                "Text": {"Data": text_message, "Charset": "UTF-8"},
             },
-        )
+        },
+    }
+
+    if reply_to:
+        kwargs["ReplyToAddresses"] = [reply_to] if isinstance(reply_to, str) else reply_to
+
+    try:
+        response = client.send_email(**kwargs)
         return response
 
     except client.exceptions.MessageRejected:
@@ -42,3 +49,16 @@ def send_email(sender, user_emails, subject, html_message):
 
     except Exception as e:
         raise ValidationError({"error": f"Unexpected Error: {e}"})
+
+
+def _html_to_plain_text(html):
+    text = re.sub(r"<br\s*/?>", "\n", html)
+    text = re.sub(r"</p>", "\n\n", text)
+    text = re.sub(r"<a[^>]*href=\"([^\"]+)\"[^>]*>[^<]*</a>", r"\1", text)
+    text = re.sub(r"<[^>]+>", "", text)
+    text = re.sub(r"&amp;", "&", text)
+    text = re.sub(r"&lt;", "<", text)
+    text = re.sub(r"&gt;", ">", text)
+    text = re.sub(r"&nbsp;", " ", text)
+    text = re.sub(r"\n{3,}", "\n\n", text)
+    return text.strip()
